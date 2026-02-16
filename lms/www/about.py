b1a1a6d2973f0fe2cf65b4c1d3f39d6a992b3777
Abdulrahman-Aleryani <abdulrahmanaleryani0@gmail.com>
2026-02-16 19:54:51 +0300

    Update about.py

--- lms/www/about.py
+++ lms/www/about.py
@@ -3,8 +3,8 @@ import frappe
 sitemap = 1
 
 def get_context(context):
-    context.doc = frappe.get_cached_doc("About Us Settings")
-    if context.doc.is_disabled:
-        frappe.local.flags.redirect_location = "/404"
-        raise frappe.Redirect
-    return context
+	context.doc = frappe.get_cached_doc("About Us Settings")
+	if getattr(context.doc, "is_disabled", False):
+		frappe.local.flags.redirect_location = "/404"
+		raise frappe.Redirect
+	return context
